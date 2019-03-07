0638ae20b97c64196253966143075babeb1b28c1
bking1989 <bradley_king@me.com>
2019-03-06 18:37:33 -0700

    Added opening animation to test file, adjusted opening text for mobile

---
+++ test/assets/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // Opening animation
+    $("span#open1").delay(800).addClass("new", 600);
+    $("span#open2").delay(1200).addClass("new", 600);
+    $("span#open3").delay(1600).addClass("new", 600);
+});
