8ced2fd2e368bf7d357985d6bc1976103122f92f
bking1989 <bradley_king@me.com>
2019-03-08 22:15:07 -0700

    Got about section started and scroll animation working

--- test/assets/js/main.js
+++ test/assets/js/main.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
     // Opening animation
-    $("span#open1").delay(800).addClass("new", 600);
-    $("span#open2").delay(1200).addClass("new", 600);
-    $("span#open3").delay(1600).addClass("new", 600);
+    $("span#open1").delay(1300).addClass("new", 600);
+    $("span#open2").delay(1700).addClass("new", 600);
+    $("span#open3").delay(2100).addClass("new", 600);
+
+    // Scroll-into-view code
+    $(window).scroll(function() {
+        let windowBottom = $(window).scrollTop() + $(window).height();
+        let aboutOffset = ($("#about").offset().top - windowBottom) * -1;
+
+        // About section fades in
+        if(aboutOffset >= 100) {
+            $("#about_content").addClass("scroll", 600);
+        }
+    });
 });
