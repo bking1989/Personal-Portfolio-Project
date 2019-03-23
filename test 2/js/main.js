4acd9ae8320aac845af2458c747d3fc079afb731
bking1989 <bradley_king@me.com>
2019-03-22 23:40:32 -0700

    Started a new prototype for the portfolio

---
+++ test 2/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(() => {
+    $("#line-one").delay(400).toggleClass("hidden", 600);
+    $("#line-two").delay(800).toggleClass("hidden", 600);
+    $("#line-three").delay(1200).toggleClass("hidden", 600);
+    $("#open-body").delay(2400).toggleClass("hidden", 600);
+    $("#open-button").delay(3000).toggleClass("hidden-button", 600);
+});
