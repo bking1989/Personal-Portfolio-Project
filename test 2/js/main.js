687b7a6a893bac60b2b5f14ce4ede060ffc127ed
bking1989 <bradley_king@me.com>
2019-03-30 11:12:57 -0700

    Fine-tuned animations

--- test 2/js/main.js
+++ test 2/js/main.js
@@ -1,13 +1,13 @@
 $(document).ready(() => {
     const baseSpeed = 500;
-    const baseDelay = 2000;
-    const inc = 200;
-    const tran = 400; 
+    const baseDelay = 2500;
+    const inc = 300;
+    const tran = 500; 
 
 
     $("#line-one").delay(baseSpeed).toggleClass("hidden", tran);
     $("#line-two").delay(baseSpeed + (inc * 1)).toggleClass("hidden", tran);
     $("#line-three").delay(baseSpeed + (inc * 2)).toggleClass("hidden", tran);
     $("#open-body").delay(baseDelay).toggleClass("hidden", tran);
-    $("#open-button").delay(baseDelay + inc).toggleClass("hidden-button", tran);
+    $("#open-button").delay(baseDelay + (inc * 2)).toggleClass("hidden-button", tran);
 });
