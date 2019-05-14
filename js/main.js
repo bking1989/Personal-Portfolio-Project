5624556368548bc04e7749eefdce45c5f9a86ca4
bking1989 <bradley_king@me.com>
2019-05-13 19:05:13 -0700

    Converted the landing animation from jQuery to pure CSS

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    // Landing Animation
-    const landingAnim = () => {
-        $("div#landing > div#guide").toggleClass("animation", 1600);
-    }
-
-    setInterval(landingAnim, 0);
-})
